987f47a155a8964ec4ecadd2aaf423463fc04a03
fisker Cheung <lionkay@gmail.com>
2023-11-07 11:13:11 +0800

    refactor: simplify `Context` (#257)

--- src/context.ts
+++ src/context.ts
@@ -2,20 +2,17 @@ import { LinesAndColumns } from 'lines-and-columns';
 
 export class Context {
   text;
-  locator;
+  #linesAndColumns!: LinesAndColumns;
+
   constructor(text: string) {
     this.text = text;
-    this.locator = new Locator(this.text);
   }
-}
 
-class Locator {
-  private _linesAndColumns: LinesAndColumns;
-  constructor(text: string) {
-    this._linesAndColumns = new LinesAndColumns(text);
-  }
-  public locationForIndex(index: number) {
-    const { line, column } = this._linesAndColumns.locationForIndex(index)!;
+  locationForIndex(index: number) {
+    this.#linesAndColumns ??= new LinesAndColumns(this.text);
+    const { line, column } = this.#linesAndColumns.locationForIndex(index)!;
     return { line: line + 1, column, index };
   }
 }
+
+export default Context;
