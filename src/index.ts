a754896fe8ab5f81ee887251265bdf1457cea66f
fisker <lionkay@gmail.com>
2023-11-07 15:08:33 +0800

    refactor: simplify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type * as ng from '@angular/compiler';
 import type * as b from '@babel/types';
 import { Context } from './context.js';
-import { type InputNode, transform } from './transform.js';
+import { transform } from './transform.js';
 import { transformTemplateBindings } from './transform-microsyntax.js';
-import type { NGMicrosyntax, NGNode, RawNGComment } from './types.js';
+import type { NGNode, RawNGComment, NGMicrosyntax } from './types.js';
 import {
   parseNgAction,
   parseNgBinding,
@@ -12,39 +12,26 @@ import {
   parseNgTemplateBindings,
 } from './utils.js';
 
-function parse(
-  input: string,
-  parseNg: (input: string) => { ast: ng.AST; comments: RawNGComment[] },
+function createParser(
+  parse: (input: string) => { ast: ng.AST; comments: RawNGComment[] },
 ) {
-  const { ast: rawNgAst, comments } = parseNg(input);
-  const context = new Context(input);
-  const _t = (n: InputNode) => transform(n, context);
-  const ast = _t(rawNgAst) as NGNode;
-  ast.comments = comments.map((comment) => _t(comment) as b.CommentLine);
-  return ast;
+  return (input: string) => {
+    const { ast: rawNgAst, comments } = parse(input);
+    const context = new Context(input);
+    const ast = transform(rawNgAst, context) as NGNode;
+    ast.comments = comments.map((comment) =>
+      transform(comment, context),
+    ) as b.CommentLine[];
+    return ast;
+  };
 }
 
-export { NGMicrosyntax, NGNode };
-
-export function parseBinding(input: string): NGNode {
-  return parse(input, parseNgBinding);
-}
-
-export function parseSimpleBinding(input: string): NGNode {
-  return parse(input, parseNgSimpleBinding);
-}
-
-export function parseInterpolationExpression(input: string): NGNode {
-  return parse(input, parseNgInterpolationExpression);
-}
-
-export function parseAction(input: string): NGNode {
-  return parse(input, parseNgAction);
-}
-
-export function parseTemplateBindings(input: string): NGMicrosyntax {
-  return transformTemplateBindings(
-    parseNgTemplateBindings(input),
-    new Context(input),
-  );
-}
+export const parseBinding = createParser(parseNgBinding);
+export const parseSimpleBinding = createParser(parseNgSimpleBinding);
+export const parseInterpolationExpression = createParser(
+  parseNgInterpolationExpression,
+);
+export const parseAction = createParser(parseNgAction);
+export const parseTemplateBindings = (input: string): NGMicrosyntax =>
+  transformTemplateBindings(parseNgTemplateBindings(input), new Context(input));
+export type { NGMicrosyntax, NGNode }
