f8f3a688705b59c3e3376c7d5a5de124b35b59a7
fisker <lionkay@gmail.com>
2023-11-14 22:10:24 +0800

    Add tests

--- tests/transform-microsyntax.test.ts
+++ tests/transform-microsyntax.test.ts
@@ -4,23 +4,26 @@ import type { NGMicrosyntaxKeyedExpression } from '../src/types.js';
 import { snapshotAst } from './helpers.js';
 
 test.each`
-  input                             | types
-  ${''}                             | ${[]}
-  ${' let hero '}                   | ${['NGMicrosyntaxLet']}
-  ${' let hero = hello '}           | ${['NGMicrosyntaxLet']}
-  ${' let hero of heroes '}         | ${['NGMicrosyntaxLet', 'NGMicrosyntaxKeyedExpression']}
-  ${' let hero ; of : heroes '}     | ${['NGMicrosyntaxLet', 'NGMicrosyntaxKeyedExpression']}
-  ${' as b '}                       | ${['NGMicrosyntaxAs']}
-  ${' a '}                          | ${['NGMicrosyntaxExpression']}
-  ${' a as b '}                     | ${['NGMicrosyntaxExpression']}
-  ${' a , b '}                      | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKey']}
-  ${' a ; b '}                      | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKey']}
-  ${' a ; b c '}                    | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKeyedExpression']}
-  ${' a ; b : c '}                  | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKeyedExpression']}
-  ${' a ; b : c as d '}             | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKeyedExpression']}
-  ${' a ; b as c '}                 | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxAs']}
-  ${' let "a" = "b" ; "c" as "d" '} | ${['NGMicrosyntaxLet', 'NGMicrosyntaxAs']}
-  ${' let "\\"" '}                  | ${['NGMicrosyntaxLet']}
+  input                                                   | types
+  ${''}                                                   | ${[]}
+  ${' let hero '}                                         | ${['NGMicrosyntaxLet']}
+  ${' let hero = hello '}                                 | ${['NGMicrosyntaxLet']}
+  ${' let hero of heroes '}                               | ${['NGMicrosyntaxLet', 'NGMicrosyntaxKeyedExpression']}
+  ${' let hero ; of : heroes '}                           | ${['NGMicrosyntaxLet', 'NGMicrosyntaxKeyedExpression']}
+  ${' as b '}                                             | ${['NGMicrosyntaxAs']}
+  ${' a '}                                                | ${['NGMicrosyntaxExpression']}
+  ${' a as b '}                                           | ${['NGMicrosyntaxExpression']}
+  ${' a , b '}                                            | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKey']}
+  ${' a ; b '}                                            | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKey']}
+  ${' a ; b c '}                                          | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKeyedExpression']}
+  ${' a ; b : c '}                                        | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKeyedExpression']}
+  ${' a ; b : c as d '}                                   | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxKeyedExpression']}
+  ${' a ; b as c '}                                       | ${['NGMicrosyntaxExpression', 'NGMicrosyntaxAs']}
+  ${' let "a" = "b" ; "c" as "d" '}                       | ${['NGMicrosyntaxLet', 'NGMicrosyntaxAs']}
+  ${' let "\\"" '}                                        | ${['NGMicrosyntaxLet']}
+  ${' let item of items; index as i; trackBy: trackByFn'} | ${[]}
+  ${' item of items; track item '}                        | ${[]}
+  ${' item of items;index as i; trackBy: trackByFn'}      | ${[]}
 `('$input', ({ input, types }) => {
   const ast = parseTemplateBindings(input);
   expect(snapshotAst(ast, input)).toMatchSnapshot();
