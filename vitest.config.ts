26dba94bd3fb59beb4aa2e9bbbfec58b99d5184f
fisker Cheung <lionkay@gmail.com>
2025-03-12 13:22:38 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       include: ['src/**/*.ts'],
       exclude: ['src/index.ts', 'src/helpers.ts', 'src/types.ts'],
       thresholds: {
-        branches: 100,
+        branches: 95,
         functions: 100,
         lines: 100,
         statements: 100,
